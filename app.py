ff27eeba4711327d5b7a2135b55ae7e8c7651ddd
rashmi-carol-dsouza <hello@rashmidsouza.com>
2022-06-09 10:06:24 +0200

    Removing input for easy testing

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 @dataclass
 class Barcode(db.Model):
     id = db.Column(db.String(200), primary_key=True)
-    date_created = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+    date_created = db.Column(db.DateTime(timezone=True), server_default=db.func.current_timestamp())
     production_line = db.Column(db.String(200), nullable=False)
 
     def to_dict(self):
